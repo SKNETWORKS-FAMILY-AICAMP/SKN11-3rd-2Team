e7908199154e11f4ea8582bb0b7f372519cbe2a0
snowFall <37786322+kicet3@users.noreply.github.com>
2025-05-15 21:25:34 +0900

    Update init.sh

--- 환경구성/init.sh
+++ 환경구성/init.sh
@@ -34,7 +34,7 @@ pyenv activate llm_env
 
 pip install --upgrade pip
 
-pip install install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
 pip install -r requirements.txt
 
